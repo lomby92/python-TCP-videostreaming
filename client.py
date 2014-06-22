ef7569faba9309074737ddae1626daa30fae0e57
Lorenzo <lorenzo.millucci@outlook.com>
2014-06-22 15:17:58 +0200

    FIX: add print error

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket, cv2, numpy
 
 client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect(('192.168.1.235',5000))
+client_socket.connect(('192.168.1.31',5000))
 
 while True:
     data=client_socket.recv(4096000)
@@ -9,8 +9,9 @@ while True:
     a1D = numpy.fromstring(data,dtype=numpy.uint8)
     try:
         img = numpy.reshape(a1D,(480,640,3))
-    except:
+    except Exception as detail:
         img= numpy.zeros((480,640,3))
+        print "Error:",detail
     print img
     print "Tipo img: ",type(img)
     print "Grandezza img",img.shape
